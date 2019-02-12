1b8c388855c72c4d7d43aba0e0baaf91d7846fbd
klanmiko <eternity01033@gmail.com>
2019-02-12 15:31:59 -0800

    fixed everything

--- resources/user.py
+++ resources/user.py
@@ -16,35 +16,6 @@ class UserResource(Resource):
 		users = users_schema.dump(users).data
 		return {'status': 'success', 'data': users}, 200
 
-	def post(self):
-		json_data = request.get_json(force=True)
-		if not json_data:
-			return {'message': 'No input data provided'}, 400
-
-		data, errors = user_schema.load(json_data)
-		if errors:
-			return errors, 422
-
-		# check how this works in docs
-		# what if I want to filter by both username and email?
-		existing_user = User.query.filter_by(username=data['username']).first()
-		if existing_user:
-			return {'message': 'User already exists'}, 400
-
-		if 'username' not in data or 'email' not in data:
-			return {'message': 'User - missing field(s)'}, 400
-
-		user = User(
-			username=data['username'],
-			email=data['email']
-			)
-		db.session.add(user)
-		db.session.commit()
-
-		result = user_schema.dump(user).data
-
-		return {'status': 'success', 'data': result}, 201
-
 	def put(self):
 		json_data = request.get_json(force=True)
 		if not json_data:
